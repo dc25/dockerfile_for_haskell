804ac05b67564f1670c6347845e625c3749bf67c
Dave Compton <davecompton7@gmail.com>
2015-10-24 23:32:12 -0700

    renamed dockerStart to start

--- start/start.sh
+++ start/start.sh
@@ -6,7 +6,7 @@ export USER_ID=$2
 sudo adduser --disabled-password --gecos '' --uid $USER_ID $USER_NAME
 sudo adduser $USER_NAME sudo
 
-sudo su $USER_NAME -c /dockerStart/personalize
+sudo su $USER_NAME -c /start/personalize
 
 echo "Starting sshd"
 sudo /usr/bin/svscan /services/
