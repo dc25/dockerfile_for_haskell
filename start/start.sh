d10ca8f0bd14c77be6944af9aa5f8cd3e33f74dc
Dave Compton <davecompton7@gmail.com>
2015-11-07 16:51:57 -0800

    made start.sh quieter

--- start/start.sh
+++ start/start.sh
@@ -3,10 +3,12 @@
 export USER_NAME=$1
 export USER_ID=$2
 
-sudo adduser --disabled-password --gecos '' --uid $USER_ID $USER_NAME
-sudo adduser $USER_NAME sudo
+echo "configuring user: $USER_NAME ..."
 
-sudo su $USER_NAME -c /start/personalize.sh
+sudo adduser --disabled-password --gecos '' --uid $USER_ID $USER_NAME > /dev/null 2>&1 
+sudo adduser $USER_NAME sudo > /dev/null 2>&1 
+
+sudo su $USER_NAME -c /start/personalize.sh > /dev/null 2>&1 
 
 echo "sshd started"
 sudo /usr/bin/svscan /services/
