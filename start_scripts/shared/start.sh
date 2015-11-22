aea96b840137af8a2d943bbb23acf7b7ca36595b
Dave Compton <davecompton7@gmail.com>
2015-11-22 11:57:36 -0800

    no longer redirecting output for personalize.sh to /dev/null.

--- start_scripts/shared/start.sh
+++ start_scripts/shared/start.sh
@@ -8,7 +8,7 @@ echo "configuring user: $USER_NAME ..."
 sudo adduser --disabled-password --gecos '' --uid $USER_ID $USER_NAME > /dev/null 2>&1 
 sudo adduser $USER_NAME sudo > /dev/null 2>&1 
 
-sudo su $USER_NAME -c /start/personalize.sh > /dev/null 2>&1 
+sudo su $USER_NAME -c /start/personalize.sh 
 
 echo "sshd started"
 sudo /usr/bin/svscan /services/
