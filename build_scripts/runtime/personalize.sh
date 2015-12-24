272295b59d1b877d106a48314c50011aa3bbb950
Dave Compton <davecompton7@gmail.com>
2015-12-24 14:48:07 -0800

    moving most of personalize.sh contents to start.sh

--- build_scripts/runtime/personalize.sh
+++ build_scripts/runtime/personalize.sh
@@ -3,25 +3,6 @@
 # This script sets up a user's environment when a docker container starts up.
 # Edit to suit your own tastes.
 
-
-BUILDER_COPY_FILES="myVimrc startup .ssh/authorized_keys .vimrc .vim .bashrc .ghc .haste"
-
-cd /home/builder/
-find $BUILDER_COPY_FILES -depth -print0 | cpio -pdum0 $HOME > /dev/null 2>&1
-chmod 600 $HOME/.ssh/authorized_keys
-
-## Link to some large configuration directories in /home/builder
-## Is the .ghcjs directory even necessary when using sandboxes?
-for s in .ghcjs ; do
-    ln -s /home/builder/$s $HOME
-done
-
-## Update cabal, creating ~/.cabal directory.  
-## Can't use /home/ghc/.cabal because cabal wants to write to this directory.
-## echo "running cabal update"
-## cabal update > /dev/null 2>&1
-
-
 ### git global configuration - customize as needed ###
 ## git config --global user.email "youremail@yourserver.com"
 ## git config --global user.name "Firstname Lastname"             
--- build_scripts/runtime/start.sh
+++ build_scripts/runtime/start.sh
@@ -4,15 +4,28 @@ export USER_NAME=$1
 export USER_ID=$2
 export USER_KEY=$3
 
-sudo su builder -c "mkdir /home/builder/.ssh"
-sudo su builder -c "echo $USER_KEY > /home/builder/.ssh/authorized_keys"
 
 echo "configuring user: $USER_NAME ..."
 
 sudo adduser --disabled-password --gecos '' --uid $USER_ID $USER_NAME > /dev/null 2>&1 
 sudo adduser $USER_NAME sudo > /dev/null 2>&1 
 
-sudo su $USER_NAME -c /home/builder/workarea/runtime/personalize.sh 
+sudo su $USER_NAME -c "mkdir \$HOME/.ssh"
+sudo su $USER_NAME -c "echo $USER_KEY > \$HOME/.ssh/authorized_keys"
+sudo su $USER_NAME -c "chmod 600 \$HOME/.ssh/authorized_keys"
+
+BUILDER_COPY_FILES="myVimrc startup .vimrc .vim .bashrc .ghc .haste"
+
+cd /home/builder/
+sudo su $USER_NAME -c "find $BUILDER_COPY_FILES -depth -print0 | cpio -pdum0 \$HOME > /dev/null 2>&1"
+
+## Link to some large configuration directories in /home/builder
+## Is the .ghcjs directory even necessary when using sandboxes?
+for s in .ghcjs ; do
+    sudo su $USER_NAME -c "ln -s /home/builder/$s \$HOME"
+done
+
+sudo su $USER_NAME -c /home/builder/workarea/runtime/personalize.sh
 
 echo "sshd started"
 sudo /usr/bin/svscan /services/
