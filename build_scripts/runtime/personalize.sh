253de8acc45248af57b50e5457131d07e45f1181
Dave Compton <davecompton7@gmail.com>
2015-12-24 15:11:54 -0800

    got rid of runtime directory

--- build_scripts/runtime/personalize.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# This script sets up a user's environment when a docker container starts up.
-# Edit to suit your own tastes.
-
-### git global configuration - customize as needed ###
-## git config --global user.email "youremail@yourserver.com"
-## git config --global user.name "Firstname Lastname"             
-
