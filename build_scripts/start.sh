24f2faa0c9c547ea3bac2c7ab6946f489585c01c
Dave Compton <davecompton7@gmail.com>
2015-12-25 21:32:28 -0800

    got rid of link to .ghcjs builder directory.

--- build_scripts/start.sh
+++ build_scripts/start.sh
@@ -19,16 +19,10 @@ BUILDER_COPY_FILES="myVimrc myBashrc .vimrc .vim .bashrc .ghc .haste"
 cd /home/builder/
 sudo su $USER_NAME -c "find $BUILDER_COPY_FILES -depth -print0 | cpio -pdum0 \$HOME > /dev/null 2>&1"
 
-## Link to some large configuration directories in /home/builder
-## Is the .ghcjs directory even necessary when using sandboxes?
-for s in .ghcjs ; do
-    sudo su $USER_NAME -c "ln -s /home/builder/$s \$HOME"
-done
-
 sudo su $USER_NAME -c "cabal update"
 
 sudo su $USER_NAME -c /home/builder/workarea/personalize.sh
 
 echo "sshd started"
 sudo /usr/bin/svscan /services/
-## sudo su $USER_NAME bash -c tmux
+## sudo su - $USER_NAME /bin/bash -c tmux
